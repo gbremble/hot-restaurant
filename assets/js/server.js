9d9ac437c164b0c1a13bcab93c32797a312b3dd0
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-07-17 20:07:38 -0400

    Update server.js and structures on reservations.html + tables.html

--- assets/js/server.js
+++ assets/js/server.js
@@ -2,9 +2,36 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
 var PORT = 3000;
+
+// Reservations (DATA)
+// =============================================================
+var reservations = [{
+    name: "Leo Daniel",
+    phoneNumber: "252-123-4567",
+    email: "leedizzle@aol.com"
+  },
+  {
+    name: "Johnny Walker",
+    phoneNumber: "123-456-7890",
+    email: "drunkguy@bar.net"
+  },
+  {
+    name: "Scott Johnson",
+    phoneNumber: "252-345-6789",
+    email: "js-guru@boss.com"
+  }
+];
+
+/**
+ * id="userName"
+ * id="userPhone"
+ * id="userEmail"
+ * id="userUID"
+ **/
 
 // Routes
 // =============================================================
@@ -18,25 +45,55 @@ app.get("/reservations", function (req, res) {
   res.sendFile(path.join(__dirname, "reservations.html"));
 });
 
-
-
-// Displays all characters
+// Displays all tables
 app.get("/tables", function (req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 // Reservations
+// Displays all reservations
+app.get("/api/reservations", function (req, res) {
+  return res.json(reservations);
+  // var reservationCount = res
+});
+
 // POST endpoint takes in JSON object
-app.post("/reservations", function (req, res) {
-      // check if there is any place left
+app.post("api/reservations", function (req, res) {
+  var newReservation = req.body;
+
+  // check if there is any place left-5 tables
+  if (reservations.length < 5) {
+
+  }
 
 
+});
 
+// =======================
 
+request.open('GET', 'url', true);
+request.onload = function () {
 
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
 
-      // Starts the server to begin listening
-      // =============================================================
-      app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
-      });
+  if (request.status >= 200 && request.status < 400) {
+    // if reservations >= 5, POST to WAITING-LIST
+
+    data.forEach(movie => {
+      console.log(movie.title);
+    });
+  } else {
+    console.log('error');
+  }
+}
+
+request.send();
+
+// ======================
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
